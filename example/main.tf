19a2dc985a636515717c8c92222929f095a7ed7c
Danny Molnar <danny.molnar@cloudreach.com>
2023-04-27 11:04:00 +0100

    chore: updated tf glob

---
+++ example/main.tf
@@ -0,0 +1,14 @@
+terraform {
+  required_version = ">= 1.3.6"
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 4.53.1"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.4.3"
+    }
+  }
+}
+
