1673d4dada6be5ec693d0317ebaa5cc2b7443bdc
Danny Molnar <danny.molnar@cloudreach.com>
2023-04-28 09:16:26 +0100

    chore: updated tf ver constraint to trigger dep

--- example/main.tf
+++ example/main.tf
@@ -3,11 +3,11 @@ terraform {
   required_providers {
     google = {
       source  = "hashicorp/google"
-      version = ">= 4.53.1"
+      version = "~> 4.53.1"
     }
     random = {
       source  = "hashicorp/random"
-      version = "3.4.3"
+      version = "~> 3.4.3"
     }
   }
 }
